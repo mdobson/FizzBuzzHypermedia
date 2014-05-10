a3b8b17a3dd49bd430d5921fc6f9cd12c5e9a612
Matt Dobson <mdobson4@gmail.com>
2014-05-10 12:14:10 -0400

    Added a link patch method to make links play nice with my client. Added fizz buzz impl.

--- FizzBuzzHypermedia/MSDViewController.m
+++ FizzBuzzHypermedia/MSDViewController.m
@@ -7,16 +7,43 @@
 //
 
 #import "MSDViewController.h"
+#import "SHMKit/SHMParser.h"
+#import "SHMKit/SHMEntity.h"
+#import "SHMKit/SHMLink.h"
 
 @interface MSDViewController ()
+
+@property SHMParser *parser;
+@property NSString *url;
 
 @end
 
 @implementation MSDViewController
 
+@synthesize parser, url;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.url = @"http://fizzbuzzaas.herokuapp.com";
+    self.parser = [[SHMParser alloc] initWithSirenRoot:self.url];
+    [self.parser retrieveRoot:^(NSError *err, SHMEntity *entity) {
+        if (err) {
+            NSLog(@"err:%@", err);
+        } else {
+            if ([entity hasLinkRel:@"first"]) {
+                [self patchLink:entity andLinkRel:@"first"];
+                [entity stepToLinkRel:@"first" withCompletion:^(NSError *error, SHMEntity *entity) {
+                    if (error) {
+                        NSLog(@"First step err:%@", error);
+                    } else {
+                        NSLog(@"Value:%@", entity.properties[@"value"]);
+                        [self fizzbuzz:entity];
+                    }
+                }];
+            }
+        }
+    }];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -24,6 +51,31 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)fizzbuzz:(SHMEntity *) entity {
+    if ([entity hasLinkRel:@"next"]) {
+        [self patchLink:entity andLinkRel:@"next"];
+        [entity stepToLinkRel:@"next" withCompletion:^(NSError *error, SHMEntity *entity) {
+            if (error) {
+                NSLog(@"Next step err:%@", error);
+            } else {
+                NSLog(@"Value:%@", entity.properties[@"value"]);
+                [self fizzbuzz:entity];
+            }
+        }];
+    }
+}
+
+-(void)patchLink:(SHMEntity *)entity andLinkRel:(NSString*)linkRel{
+    for (SHMLink *link in entity.links) {
+        for (NSString *rel in link.rel) {
+            if ([rel isEqualToString:linkRel]) {
+                NSString *patchedLink = [NSString stringWithFormat:@"%@%@", self.url, link.href];
+                link.href = patchedLink;
+            }
+        }
+    }
 }
 
 @end
